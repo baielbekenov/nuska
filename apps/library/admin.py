427fd6d7090087023b5e702fa2abfca6965f8d09
baiel bekenov <baelbekbolotovb12356@gmail.com>
2023-12-01 11:58:46 +0600

    added params

--- apps/library/admin.py
+++ apps/library/admin.py
@@ -6,15 +6,5 @@ admin.site.register(Author)
 admin.site.register(Jenre)
 admin.site.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    resource_class = Book
-    fieldsets = (
-        ("general", {"fields": ("name", "author", "jenre")}),
-        ("other", {"fields": ("created_at", "rating", "isbn", "published_on")}),
-    )
-    list_filter = ("title",)
-
-    # Render filtered options only after 5 characters were entered
-    filter_input_length = {
-        "title": 5,
-    }
+    list_display = ('name', 'author') 
 admin.site.register(Comment)
