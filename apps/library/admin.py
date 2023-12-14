91e3cdc5aaa4657fc7194b3e2e0d8290d50bd9f0
baielbekenov <baelbekbolotovb@gmail.com>
2023-12-14 23:47:42 +0600

    added new

--- apps/library/admin.py
+++ apps/library/admin.py
@@ -2,42 +2,15 @@ from django.contrib import admin
 from apps.library.models import Author, Jenre, Book, Comment
 from apps.library.modelfilter import  JenreFilter
 from import_export.admin import ImportExportModelAdmin
-import xlwt
 
 admin.site.register(Author)
 admin.site.register(Jenre)
-
-
-def export_as_excel(modeladmin, request, queryset):
-    response = HttpResponse(content_type='application/ms-excel')
-    response['Content-Disposition'] = 'attachment; filename="report.xls"'
-
-    wb = xlwt.Workbook(encoding='utf-8')
-    ws = wb.add_sheet("Book")
-
-    row_num = 0
-    columns = [field.name for field in modeladmin.model._meta.fields]
-
-    for col_num in range(len(columns)):
-        ws.write(row_num, col_num, columns[col_num])
-
-    for obj in queryset:
-        row_num += 1
-        for col_num in range(len(columns)):
-            ws.write(row_num, col_num, getattr(obj, columns[col_num]))
-
-    wb.save(response)
-    return response
-
-export_as_excel.short_description = "Export Selected"
-
 
 @admin.register(Book)
 class BookAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('name', 'author', 'display_jenres')
     search_fields = ('name', )
     list_filter = (JenreFilter,)
-    actions = [export_as_excel]
 
     def display_jenres(self, obj):
         """Возвращает строку со всеми жанрами книги."""
